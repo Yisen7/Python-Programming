db0c85e1f2bc4c247685b58744fa059a40a5fb25
Yisen <13212152757@163.com>
2019-11-05 20:52:14 +0800

    爬取“今日头条”页面数据-title

--- src/code/testDemo/testspider.py
+++ src/code/testDemo/testspider.py
@@ -1,4 +1,14 @@
 import requests
+import json
+
 if __name__ == '__main__':
     # 目标url
-    url = "https://www.toutiao.com/ch/news_hot/"
+    url = "https://www.toutiao.com/api/pc/realtime_news/"
+    # 发送请求，获取服务器响应数据
+    response = requests.get(url)
+    results = response.text
+    resultData = json.loads(results)
+    # 将标题全部取出， resultData["data"] ---列表[字典]
+    for data in resultData["data"]:
+        print(data["title"])
+
